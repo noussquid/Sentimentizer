192765aaf9f882e6cd2d2270473fe0d82591fd66
noussquid <karrliii@gmail.com>
2013-11-19 00:38:47 -0800

    Trying to submit a form and failing

--- index.php
+++ index.php
@@ -4,13 +4,23 @@
 <head>
 	<meta charset="utf-8">
 	<title>sentimentizer</title>
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="css/style.css">
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-	<script type="text/javascript">		
-		$(function(){ 
+	<script type="text/javascript">
+	console.log("hello?");
+
+	
+	// Attach a submit handler to the form
+	//$("#foo").submit(function(event) {		
+	$(function(){
+
+		console.log("Am I inside this function?!");	
+		// stop the form from submitting normally
+		event.preventDefault();
 		
-			//send the ajax request
-			$.ajax({
+		console.log("this is happening?");
+
+		$.ajax({
 				url: "http://sentimentizer.herokuapp.com/", //url to hit
 				dataType: "json", //type of data expected back from server {status:success}
 				data: {analyze: "Squid got this thing working even though she's flailing like a n00b."},
@@ -25,18 +35,22 @@
 					$('#messages').append('<div class="error">'+errorThrown+'</div>');
 				},
 			});
+
+		});
+
+		$('#fooBtn').click(function(){
+			$('#foo').submit();
+		});	
 			
-			$('#messages').text('fuck javascript and modern coding');
-		}); 
 	</script>
 	
 </head>
 <body>
-	<!-- <div id="messages"></div> -->
-	<form name="messageForm" method="post" action="">
-		<input type="text" name="message" id="message" />
-		<input type="submit" id="submit" value="Submit" />
+	<form id="foo">
+		<input type="text" name="message" id="message" value="" />
+		<input type="submit" value="Sentimentize" />
 	</form>
+	<div id="messages"></div>
 	<div id="sentence"></div>
 	<div id="polarity"></div>
 	<div id="subjectivity"></div>
